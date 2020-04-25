28807e1ce57e70cad848e91f5e89c719b4ab1b1e
programacion <49229873+mpuertob@users.noreply.github.com>
2020-04-25 23:51:06 +0200

    empezandoEjercicio

--- src/pruebasui/MercadoSoldadosPrueba.java
+++ src/pruebasui/MercadoSoldadosPrueba.java
@@ -9,15 +9,18 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import control.PobladorController;
 import modelo.Batallon;
 import modelo.Tipo;
 import vista.MercadoSoldadoDialog;
+import vista.Conversores.Generador;
 import vista.info.MercadoSoldadoInfo;
 
 public class MercadoSoldadosPrueba extends JFrame {
 
 	private JPanel contentPane;
-	MercadoSoldadoDialog mercadoSoldadoPruebaDialog;
+	MercadoSoldadoDialog mercadoSoldadoDialog;
+	PobladorController pobladorController;
 	/**
 	 * Launch the application.
 	 */
@@ -42,16 +45,32 @@ public class MercadoSoldadosPrueba extends JFrame {
 		setBounds(100, 100, 678, 479);
 		
 		JButton btnDialogo = new JButton("dialogo");
+		Batallon batallon = new Batallon(1, Tipo.infanteria);				
+		mercadoSoldadoDialog=new MercadoSoldadoDialog(Generador.getMercadoSoldadoInfo(batallon));
 		btnDialogo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Batallon batallon = new Batallon(1, Tipo.infanteria);
-				MercadoSoldadoInfo info = new MercadoSoldadoInfo(batallon);
-				mercadoSoldadoPruebaDialog=new MercadoSoldadoDialog(info);
-				mercadoSoldadoPruebaDialog.setVisible(true);
+				mercadoSoldadoDialog.setVisible(true);
 			}
 		});
 		getContentPane().add(btnDialogo, BorderLayout.CENTER);
-		
+		//Esto va a aconectar el ui con el modelo de datos
+		pobladorController=new PobladorController(new Batallon(1, Tipo.arqueria));
+		getBtnOk().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {		
+//				MercadoSoldadoDialog dialogo=(MercadoSoldadoDialog)e.getSource();
+				if(mercadoSoldadoDialog.compruebaMax()) {
+					//aqui es donde hay que conectar con el modelo de datos
+					pobladorController.poblarBatallon(mercadoSoldadoDialog.getListaEjercito());
+				}
+				dispose();
+			}
+		});
+	}
+
+	public JButton getBtnOk() {
+		return mercadoSoldadoDialog.getBtnOk();
 	}
 
 }
--- src/vista/Conversores/Generador.java
+++ src/vista/Conversores/Generador.java
@@ -1,13 +1,26 @@
 package vista.Conversores;
 
+import java.awt.Color;
 import java.awt.event.FocusAdapter;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.swing.JPanel;
+
+import modelo.Batallon;
+import modelo.Casilla;
+import modelo.Coordenada;
+import modelo.Ejercito;
 import modelo.Especialidad;
+import modelo.Tablero;
 import modelo.Tipo;
 import vista.EspecialidadSoldado;
+import vista.Ficha;
+import vista.FichaBlanca;
+import vista.info.EjercitoInfo;
 import vista.info.EspecialidadSoldadoInfo;
+import vista.info.FichaInfo;
+import vista.info.MercadoSoldadoInfo;
 
 public class Generador {
 
@@ -21,5 +34,31 @@ public class Generador {
 		return panelesEspecialidadSoldadosEnsayos;
 	}
 
+	public static MercadoSoldadoInfo getMercadoSoldadoInfo(Batallon batallon) {
+		return new MercadoSoldadoInfo(batallon);
+	}
 
+	public static EjercitoInfo getEjercitoInfo(Ejercito ejercito) {
+		return new EjercitoInfo(ejercito.getId(), ejercito.getIdBatallonActual(), ejercito.getTipoBatallon(),
+				ejercito.getInfanteria(), ejercito.getCaballeria(), ejercito.getArqueria());
+	}
+
+	private static FichaInfo getFichaInfo(Tablero tablero, Coordenada coordenada) {
+		Casilla casilla = tablero.getCasilla(coordenada);
+		FichaInfo fichaInfo=null;
+		if(casilla!=null) {
+			Batallon batallon=(Batallon)casilla;
+			//TODO cambiar cuando el batallon tenga todos los valores para la ficha
+			fichaInfo=new FichaInfo("/Imagenes/ligera.png", -1, batallon.getId(), -1,
+					-1, -1, -1, batallon.getMaximoSoldados(), false, Color.BLACK);
+		}
+		return fichaInfo;
+	}
+	public static JPanel getFicha(Tablero tablero, Coordenada coordenada) {
+		FichaInfo fichaInfo=getFichaInfo(tablero, coordenada);
+		if(fichaInfo==null) {
+			return new FichaBlanca();
+		}
+		return new Ficha(fichaInfo);
+	}
 }
